65fbfe6f0cba4139aeb96ada02f919782c4a839d
drag <mi.e.drago@gmail.com>
2020-06-11 20:11:17 +0300

    Updates

---
+++ precache-manifest.6bb91f06ff0121cbbe1dc0ddd8bc96f5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7491c77388b67bccf1ff22d0e825dda1",
+    "url": "/dragora-playlist-generator/index.html"
+  },
+  {
+    "revision": "0a045c9812e941bbab0a",
+    "url": "/dragora-playlist-generator/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5c1d672a58b643ec9d02",
+    "url": "/dragora-playlist-generator/static/js/13.280de326.chunk.js"
+  },
+  {
+    "revision": "eab68b228ffe167b829a4038048ddade",
+    "url": "/dragora-playlist-generator/static/js/13.280de326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3de69f2a5c3f391bd65",
+    "url": "/dragora-playlist-generator/static/js/14.9c1565fa.chunk.js"
+  },
+  {
+    "revision": "0a045c9812e941bbab0a",
+    "url": "/dragora-playlist-generator/static/js/main.88800fa8.chunk.js"
+  },
+  {
+    "revision": "4921cfb5d868ebe7fdfd",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerDailyMotion.cd6788ba.chunk.js"
+  },
+  {
+    "revision": "cac4447cf87a76d4fdd5",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerFacebook.1b87a15f.chunk.js"
+  },
+  {
+    "revision": "d555b56c6d3722b4bd56",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerFilePlayer.3dea6229.chunk.js"
+  },
+  {
+    "revision": "86391073441e38221b4e",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerMixcloud.628dd827.chunk.js"
+  },
+  {
+    "revision": "84db95af573b9c7c4f05",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerSoundCloud.81d60883.chunk.js"
+  },
+  {
+    "revision": "b27496ab036994a273ff",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerStreamable.6e8e162e.chunk.js"
+  },
+  {
+    "revision": "ceb38795b8cefea351cd",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerTwitch.15643331.chunk.js"
+  },
+  {
+    "revision": "6294b3618a2ce2de8fdd",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerVidyard.4a6a7eee.chunk.js"
+  },
+  {
+    "revision": "8084289dacfa6ff74cb1",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerVimeo.917313fb.chunk.js"
+  },
+  {
+    "revision": "54277fccc1403379ec83",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerWistia.c4c1cb65.chunk.js"
+  },
+  {
+    "revision": "ddb3f37ad1507cf58687",
+    "url": "/dragora-playlist-generator/static/js/reactPlayerYouTube.a08d8410.chunk.js"
+  },
+  {
+    "revision": "94d9ec14c865a5fbb510",
+    "url": "/dragora-playlist-generator/static/js/runtime-main.a6f351ad.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dragora-playlist-generator/static/media/logo.25bf045c.svg"
+  }
+]);
